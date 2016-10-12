7e0ab3dcd2cc418ba85e6d2b2486a432a51e5801
Dmitry Rumyantsev <lemtoup@gmail.com>
2016-10-12 16:15:27 +0300

    Replace outside $helper->can("Create" ) checking

--- com_testimonials/site/views/testimonials/tmpl/flipped_corner.php
+++ com_testimonials/site/views/testimonials/tmpl/flipped_corner.php
@@ -123,16 +123,17 @@ JFactory::getDocument()->addStyleDeclaration("
 		<div class="text-right">
 			<a href="<?php echo JRoute::_('index.php?option=com_testimonials&view=form'.$tmpl.($cat_id?'&catid='.$cat_id:'').'&Itemid='.TestimonialsHelper::getClosesItemId('index.php?option=com_testimonials&view=form'.($cat_id?'&catid='.$cat_id:''))); ?>" class="modal_com_testim btn btn-success" rel="{handler:'iframe',size:{x: (0.8*((jQuery('.testimonials-layout').width())?jQuery('.testimonials-layout').width():jQuery('.container').width())), y: (0.8*jQuery(window).height())}}"><?php echo (JText::_('COM_TESTIMONIALS_ADD')); ?></a><br />
 		</div>
-		<div class="text-right">
-			<fieldset id="jform_MetaAuthor" class="btn-group btn-group-yesno radio">
-				<input type="hidden" id="order" name="ordering" value="t_caption">
-				<label for="order" class="btn <?php echo $this->order['name'] ?>" onclick="document.adminForm.ordering.value='t_caption'; document.adminForm.submit();"><?php echo JTEXT::_('COM_TESTIMONIALS_TOPIC_BY_NAME'); ?></label>
-				<label for="order" class="btn <?php echo $this->order['order'] ?>" onclick="document.adminForm.ordering.value='ordering'; document.adminForm.submit();"><?php echo JTEXT::_('COM_TESTIMONIALS_TOPIC_BY_ORDER'); ?></label>
-			</fieldset>
-		</div>
 	<?php
 	}
 ?>
+	<div class="text-right">
+		<fieldset id="jform_MetaAuthor" class="btn-group btn-group-yesno radio">
+			<input type="hidden" id="order" name="ordering" value="t_caption">
+			<label for="order" class="btn <?php echo $this->order['name'] ?>" onclick="document.adminForm.ordering.value='t_caption'; document.adminForm.submit();"><?php echo JTEXT::_('COM_TESTIMONIALS_TOPIC_BY_NAME'); ?></label>
+			<label for="order" class="btn <?php echo $this->order['order'] ?>" onclick="document.adminForm.ordering.value='ordering'; document.adminForm.submit();"><?php echo JTEXT::_('COM_TESTIMONIALS_TOPIC_BY_ORDER'); ?></label>
+		</fieldset>
+	</div>
+	
 	<div class="testimonials-list">
 		<?php
 			echo $this->loadTemplate('items');
